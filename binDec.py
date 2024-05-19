486930c4f53465f20d803b0fd06781275dec1e98
YourGJuggernaut <69082957+YourGJuggernaut@users.noreply.github.com>
2024-05-19 20:25:54 +0200

    Updated Security

--- binDec.py
+++ binDec.py
@@ -1,10 +1,10 @@
 from numbin import bintonum # type: ignore
 bol = False
 
-with open('DataDE.txt', 'w') as file:
+with open('DataDE.pycomm', 'w') as file:
     pass
 
-with open('DataEN.txt', 'r') as file:
+with open('DataEN.pycomm', 'r') as file:
     content = file.read()
 # CHAT GPT START ---
 # Split the binary string into groups of 5 bits
@@ -32,6 +32,6 @@ for chunk in binary_chunks:
             decoded_string += chr(decimal_value + 97)
     # - mine
 # Write the decoded string to DataDE.txt
-with open('DataDE.txt', 'a', encoding='utf-8') as file:
+with open('DataDE.pycomm', 'a', encoding='utf-8') as file:
     file.write(decoded_string)
 # CHAT GPT END ---
